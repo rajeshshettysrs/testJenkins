40275a33ee0de9ffe1504541ca75d84bcfbfd090
Rajesh <rajeshshetty718@gmail.com>
2019-11-26 21:09:45 +0530

    Update Program.cs
    test

--- TestJenkins/Program.cs
+++ TestJenkins/Program.cs
@@ -14,7 +14,8 @@ namespace TestJenkins
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            
+		Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new fromDownload());
         }
     }
